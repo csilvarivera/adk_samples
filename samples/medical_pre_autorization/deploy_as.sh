a628f9f8595e4b7e7b2a3618fd9dc29c7055a59b
Christian Silva <csilvarivera@google.com>
2025-09-25 08:42:24 +0100

    medical_pre_autorization agent

---
+++ samples/medical_pre_autorization/deploy_as.sh
@@ -0,0 +1,34 @@
+export AGENT_DISPLAY_NAME="Medical Pre-Authorization"
+export AGENT_DESCRIPTION="Medical Pre-Authorization Demo for Cymbal "
+export AGENT_ID="Medical_Pre_Authorization"
+export AS_APP="oem_1746532905481"
+export REASONING_ENGINE="projects/774298971519/locations/us-central1/reasoningEngines/8540113521122213888"
+
+export PROJECT_ID="csilvariverademo"
+export PROJECT_NUMBER="774298971519"
+
+
+echo "REASONING_ENGINE: $REASONING_ENGINE"
+echo "PROJECT_NUMBER: $PROJECT_NUMBER"
+echo "PROJECT_ID: $PROJECT_ID"
+
+curl -X POST \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H "Content-Type: application/json" \
+-H "X-Goog-User-Project: ${PROJECT_ID}" \
+  "https://discoveryengine.googleapis.com/v1alpha/projects/${PROJECT_NUMBER}/locations/global/collections/default_collection/engines/${AS_APP}/assistants/default_assistant/agents" \
+-d '{
+      "displayName": "'"${AGENT_DISPLAY_NAME}"'",
+      "description": "'"${AGENT_DESCRIPTION}"'",
+      "icon": {
+        "uri": "https://fonts.gstatic.com/s/i/short-term/release/googlesymbols/corporate_fare/default/24px.svg"
+      },
+      "adk_agent_definition": {
+        "tool_settings": {
+          "tool_description": "'"${AGENT_DESCRIPTION}"'",
+        },
+        "provisioned_reasoning_engine": {
+        "reasoning_engine":"'"${REASONING_ENGINE}"'"
+        }
+      },    
+}'
---
+++ samples/medical_pre_autorization/medical_pre_autorization/sub_agents/__init__.py
@@ -0,0 +1,3 @@
+from .information_extractor.agent import information_extractor
+from .data_analyst.agent import data_analyst
+__all__ = ["data_analyst","information_extractor"]
